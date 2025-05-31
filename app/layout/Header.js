257da658def52f35939f02ee0940db591939f176
Dy <gdy1218@gmail.com>
2025-05-31 21:56:23 +0200

    change infite scoll to pagination

--- app/layout/Header.js
+++ app/layout/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='border flex justify-between w-full text-center'>
       <Link href='/' className='m-6 text-3xl'>GPTs Store</Link>
-      <a href='https://github.com/DyanGao' target="_blank" className='m-6'>
+      <a href='https://github.com/DyanGao/gpts_store' target="_blank" className='m-6'>
         <Image src={githubIcon} alt="github" width={32} height={32} />
       </a>
     </div>
--- app/page.js
+++ app/page.js
@@ -176,7 +176,7 @@ export default function Home() {
                           className="p-4 mt-4 bg-white rounded border-2 shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)] hover:scale-y-105 hover:scale-x-105 active:translate-x-[4px] active:translate-y-[4px] active:shadow-none"
                           key={`${gpt.id || index}-search-${searchPagination.currentPage}`}
                         >
-                          <Link href={gpt.URL || ''}>
+                          <Link href={gpt.URL || ''} target="_blank" rel="noopener noreferrer">
                             <h2 className="text-2xl">{gpt.Name}</h2>
                             <p className="text-slate-500">{gpt.Description}</p>
                           </Link>
